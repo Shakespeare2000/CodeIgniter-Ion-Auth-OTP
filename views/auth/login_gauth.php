e2a2a66a041343536d371edf4e0e58407c866689
SpyTec <spytec13@gmail.com>
2014-04-12 00:44:21 +0200

    Deleted extra code

--- views/auth/login_gauth.php
+++ views/auth/login_gauth.php
@@ -1,32 +1,27 @@
-<section id="main" class="main-padding">
-    <h1><?php echo lang('login_heading');?></h1>
-    <p><?php echo lang('login_subheading');?></p>
-    
-    <div id="infoMessage"><?php echo $message;?></div>
+<h1><?php echo lang('login_heading');?></h1>
+<p><?php echo lang('login_subheading');?></p>
 
-    <?php echo form_open("auth/login_gauth");?>
+<div id="infoMessage"><?php echo $message;?></div>
 
-      <p>
-        <?php echo "Token: "/*lang('login_identity_label', 'identity')*/;?>
-        <?php echo form_input($token);?>
-      </p>
+<?php echo form_open("auth/login_gauth");?>
 
-      <p>
-        <?php /*echo lang('login_password_label', 'password');*/?>
-        <?php echo form_input($identity);?>
-      </p>
+  <p>
+    <?php echo "Token: "/*lang('login_identity_label', 'identity')*/;?>
+    <?php echo form_input($token);?>
+  </p>
 
-      <p>
-        <?/*php echo lang('login_remember_label', 'remember');*/?>
-        <?php echo form_input($remember);?>
-      </p>
+  <p>
+    <?php echo form_hidden($identity);?>
+  </p>
 
-      <p>
-        <?/*php echo lang('login_remember_label', 'remember');*/?>
-        <?php echo form_input($gauth_login_key);?>
-      </p>
+  <p>
+    <?php echo form_hidden($remember);?>
+  </p>
 
-      <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+  <p>
+    <?php echo form_hidden($gauth_login_key);?>
+  </p>
 
-    <?php echo form_close();?>
-</section>
+  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+
+<?php echo form_close();?>
