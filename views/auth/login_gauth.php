b04e2806c1e0460949b38d5aedae4df2ca932854
SpyTec <spytec13@gmail.com>
2014-04-12 12:31:50 +0200

    Added correct language lines
    Changed form_hidden to form_input for the time being (it's still hidden
    though)

--- views/auth/login_gauth.php
+++ views/auth/login_gauth.php
@@ -1,25 +1,25 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<h1><?php echo lang('gauth_login_heading');?></h1>
+<p><?php echo lang('gauth_login_subheading');?></p>
 
 <div id="infoMessage"><?php echo $message;?></div>
 
 <?php echo form_open("auth/login_gauth");?>
 
   <p>
-    <?php echo "Token: "/*lang('login_identity_label', 'identity')*/;?>
+    <?php echo lang('gauth_login_token_label', 'token');?>
     <?php echo form_input($token);?>
   </p>
 
   <p>
-    <?php echo form_hidden($identity);?>
+    <?php echo form_input($identity);?>
   </p>
 
   <p>
-    <?php echo form_hidden($remember);?>
+    <?php echo form_input($remember);?>
   </p>
 
   <p>
-    <?php echo form_hidden($gauth_login_key);?>
+    <?php echo form_input($gauth_login_key);?>
   </p>
 
   <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
